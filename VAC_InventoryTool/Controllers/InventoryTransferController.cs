f1ba8965f7b9dfef9e337924d3c48131a85f688b
Le Hong Thai <thai.le@action-composites.com.vn>
2020-05-12 15:14:07 +0700

    first commit

---
+++ VAC_InventoryTool/Controllers/InventoryTransferController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SAPbobsCOM;
+
+namespace VAC_InventoryTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryTransferController : ControllerBase
+    {
+        // POST: api/inventorytransfer
+        [HttpPost]
+        public void Post([FromBody] string fromWarehouse, string toWarehouse, string itemCode, double quantity)
+        {
+            StockTransfer doc = new StockTransfer();
+            doc.DocDate = DateTime.Now;
+            doc.FromWarehouse = fromWarehouse;
+            doc.ToWarehouse = toWarehouse;
+
+            doc.Lines.SetCurrentLine(0);
+            doc.Lines.ItemCode = itemCode;
+            doc.Lines.FromWarehouseCode = fromWarehouse;
+            doc.Lines.WarehouseCode = toWarehouse;
+            doc.Lines.Quantity = quantity;
+        }
+
+    }
+}
---
+++ VAC_InventoryTool/Controllers/LogInController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SAPbobsCOM;
+
+namespace VAC_InventoryTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogInController : ControllerBase
+    {
+        // POST: api/login
+        [HttpGet]
+        public IActionResult Post([FromBody] string username, string password)
+        {
+            Company VAC = new Company();
+            try
+            {
+                VAC.CompanyDB = "AC_Test";
+                VAC.Server = "SAPMAIN";
+                VAC.LicenseServer = "SAPMAIN:30000";
+                VAC.SLDServer = "SAPMAIN:40000";
+                VAC.DbUserName = "sa";
+                VAC.DbPassword = "ACVN1234~!";
+                VAC.UserName = username;
+                VAC.Password = password;
+                VAC.DbServerType = BoDataServerTypes.dst_MSSQL2016;
+                VAC.UseTrusted = false;
+                int ret = VAC.Connect();
+                string errMsg = VAC.GetLastErrorDescription();
+                int errNo = VAC.GetLastErrorCode();
+                if (errNo != 0)
+                {
+                    return StatusCode(200);
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
